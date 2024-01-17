02d4bbe80e7da8a5a28e6f09ea631877e923b35e
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-01-16 23:00:03 -0500

    Update ipfs.py
    New with project IDs

--- ipfs.py
+++ ipfs.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def pin_to_ipfs(data):
-    assert isinstance(data, dict), "Error pin_to_ipfs expects a dictionary"
+    assert isinstance(data, dict), "Error: pin_to_ipfs expects a dictionary"
 
     # Convert the dictionary to a JSON string
     json_data = json.dumps(data)
@@ -10,8 +10,15 @@ def pin_to_ipfs(data):
     # Define the endpoint for pinning to IPFS
     ipfs_url = 'https://ipfs.infura.io:5001/api/v0/add'
 
+    infura_project_id = '8e029658709540f6bc1fb9b8f0a270c8'
+    infura_project_secret = '3V+HceQMSpSdLgVgLVIvc9TLb/hzrjDihlqA8XtpJOvrU4KGtmBdcw'
+    # Headers for basic authentication
+    headers = {
+        'Authorization': 'Basic ' + base64.b64encode(f"{infura_project_id}:{infura_project_secret}".encode()).decode()
+    }
+
     # Send a POST request with the JSON data
-    response = requests.post(ipfs_url, files={"file": json_data})
+    response = requests.post(ipfs_url, headers=headers, files={"file": json_data})
     response.raise_for_status()  # This will raise an error for a failed request
 
     # Extract the CID from the response
