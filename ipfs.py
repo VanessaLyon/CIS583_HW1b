49de83baee9a5611020cf4b55433fc6f1fe911e4
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-01-17 07:47:50 -0500

    Update ipfs.py
    change on url and json read

--- ipfs.py
+++ ipfs.py
@@ -1,11 +1,15 @@
 import requests
 import json
+import io
 
 def pin_to_ipfs(data):
     assert isinstance(data, dict), "Error: pin_to_ipfs expects a dictionary"
 
     # Convert the dictionary to a JSON string
     json_data = json.dumps(data)
+
+    # Convert JSON string to a bytes stream
+    json_bytes = io.BytesIO(json_data.encode())
 
     # Define the endpoint for pinning to IPFS
     ipfs_url = 'https://ipfs.infura.io:5001/api/v0/add'
@@ -14,17 +18,15 @@ def pin_to_ipfs(data):
     project_secret = '3V+HceQMSpSdLgVgLVIvc9TLb/hzrjDihlqA8XtpJOvrU4KGtmBdcw'
 
     files = {
-    'file': data
+        'file': ('data.json', json_bytes)  # Use a tuple (filename, fileobj)
     }
 
     # Send a POST request with the JSON data
-    response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(project_id,project_secret))
-    print(response.text)
-
+    response = requests.post(ipfs_url, files=files, auth=(project_id, project_secret))
     response.raise_for_status()  # This will raise an error for a failed request
 
     # Extract the CID from the response
-    cid = response.text
+    cid = response.json()["Hash"]
 
     return cid
 
@@ -32,7 +34,7 @@ def get_from_ipfs(cid, content_type="json"):
     assert isinstance(cid, str), "get_from_ipfs accepts a cid in the form of a string"
 
     # Define the endpoint for retrieving from IPFS
-    ipfs_gateway_url = f'https://ipfs.infura.io/ipfs/cat?arg={cid}'
+    ipfs_gateway_url = f'https://ipfs.infura.io:5001/api/v0/cat?arg={cid}'
 
     # Send a GET request
     response = requests.get(ipfs_gateway_url)
